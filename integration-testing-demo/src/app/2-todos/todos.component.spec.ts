8c09d9a3a798ea6778332cbf4bebd291f59d876a
juanGro <ing.juangro@gmail.com>
2017-07-12 01:13:01 -0500

    Dealing with Asynchronous Operations chapter and Finish the course

--- integration-testing-demo/src/app/2-todos/todos.component.spec.ts
+++ integration-testing-demo/src/app/2-todos/todos.component.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, fakeAsync, tick, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 import { HttpModule } from '@angular/http';
@@ -38,13 +38,31 @@ describe('TodosComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should load todos from the server', () => {
+  // First way
+  it('should load todos from the server', async(() => {
     let service = TestBed.get(TodoService);
     // fixture.debugElement.injector.get(TodoService);
-    // spyOn(service, 'getTodos').and.returnValue(Observable.from([ [1, 2, 3] ]));
+    spyOn(service, 'getTodosPromise').and.returnValue(Promise.resolve([1, 2, 3]));
 
     fixture.detectChanges();
 
-    // expect(component.todos.length).toBe(3);
-  });
+    fixture.whenStable().then(() => {
+      expect(component.todos.length).toBe(3);
+    });
+
+    // console.log("EXPECT WAS CALLED");
+  }));
+
+  // Second way
+  it('should load todos from the server', fakeAsync(() => {
+    let service = TestBed.get(TodoService);
+    // fixture.debugElement.injector.get(TodoService);
+    spyOn(service, 'getTodosPromise').and.returnValue(Promise.resolve([1, 2, 3]));
+
+    fixture.detectChanges();
+
+    tick();
+
+    expect(component.todos.length).toBe(3);
+  }));
 });

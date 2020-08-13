e22dd7c4c2125388517e9618aee7eb78d46f4233
BienNguyen <thebienpronguyen2201@gmail.com>
2020-08-13 16:25:58 +0700

    Fix testing when cancel delete a todo

--- testing-demo/src/app/06-services/todos.component.spec.ts
+++ testing-demo/src/app/06-services/todos.component.spec.ts
@@ -56,7 +56,7 @@ describe('TodosComponent', () => {
 
   it('should NOT call the server to delete a todo item if the user cancels', () => {
     spyOn(window, 'confirm').and.returnValue(false);
-    let spy = spyOn(service, 'delete').and.returnValue(Observable.empty);
+    let spy = spyOn(service, 'delete').and.returnValue(Observable.empty());
 
     component.delete(1);
 
